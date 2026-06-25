11870b3cd25cf54d8bbc24e1c9b24114ea6d90e6
Alfa bita <siyamnurrahman@gmail.com>
2026-06-25 22:32:47 +0600

    Added revision question generation

--- implementation/test.cpp
+++ implementation/test.cpp
@@ -1,29 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
-
 int main(){
-    int n;
-    cin >> n;
-    int h[n];
-    int a[n];
-    for(int i = 0; i < n; i++){
+    int tc;
+    cin >> tc;
+    for(int i = 0; i < tc; i++){
         int x;
         cin >> x;
-        h[i] = x;
-        int y;
-        cin >> y;
-        a[i] = y;
-    }
-    int cnt = 0;
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            if(h[i] == a[j]){
-                cnt++;
-            }
+        if(x % 3 == 0){
+            cout<<"Second"<<endl;
+        }
+        else{
+            cout<<"First"<<endl;
         }
     }
-
-    cout<<cnt<<endl;
 
     return 0;
 }
